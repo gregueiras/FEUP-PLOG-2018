f2a1b6092fee17db27054731f148bc92cdcffbf6
Gonçalo Santos <gregueiras@gmail.com>
2018-10-16 10:39:54 +0100

    Cells represented as terms

---
+++ Manalath/test.pl
@@ -0,0 +1,419 @@
+:- use_module(library(lists)).
+
+cell(0, 0, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(2, 0, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(4, 0, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(6, 0, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(8, 0, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+
+ 
+cell(1, 1, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(3, 1, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(5, 1, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(7, 1, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(9, 1, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(11, 1, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+
+ 
+cell(0, 2, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(2, 2, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(4, 2, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(6, 2, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(8, 2, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(10, 2, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(12, 2, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+
+ 
+cell(1, 3, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(3, 3, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(5, 3, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(7, 3, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(9, 3, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(11, 3, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(13, 3, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(15, 3, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+
+ 
+cell(0, 4, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(2, 4, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(4, 4, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(6, 4, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(8, 4, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(10, 4, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(12, 4, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(14, 4, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(16, 4, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+
+ 
+cell(1, 5, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(3, 5, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(5, 5, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(7, 5, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(9, 5, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(11, 5, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(13, 5, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(15, 5, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+
+ 
+cell(0, 6, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(2, 6, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(4, 6, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(6, 6, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(8, 6, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(10, 6, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(12, 6, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+
+ 
+cell(1, 7, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(3, 7, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(5, 7, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(7, 7, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(9, 7, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(11, 7, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+
+ 
+cell(0, 8, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(2, 8, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(4, 8, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(6, 8, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece. 
+cell(8, 8, Piece) :- 
+ Piece = emptyCell; 
+ Piece = blackPiece; 
+ Piece = whitePiece.
+
+neighbor(X, Y, _P, F) :-
+  cell(X, Y, _P),
+  X1 is X - 1,
+  Y1 is Y - 1,
+  cell(X1, Y1, F).
+
+neighbor(X, Y, _P, F) :-
+  cell(X, Y, _P),
+  X1 is X + 1,
+  Y1 is Y - 1,
+  cell(X1, Y1, F).
+
+neighbor(X, Y, _P, F) :-
+  cell(X, Y, _P),
+  X1 is X + 2,
+  Y1 is Y,
+  cell(X1, Y1, F).
+
+neighbor(X, Y, _P, F) :-
+  cell(X, Y, _P),
+  X1 is X - 1,
+  Y1 is Y + 1,
+  cell(X1, Y1, F).
+
+neighbor(X, Y, _P, F) :-
+  cell(X, Y, _P),
+  X1 is X - 2,
+  Y1 is Y,
+  cell(X1, Y1, F).
+
+board([
+cell(0, 0, emptyCell),
+cell(2, 0, emptyCell),
+cell(4, 0, emptyCell),
+cell(6, 0, emptyCell),
+cell(8, 0, emptyCell), 
+
+cell(1, 1, emptyCell), 
+cell(3, 1, emptyCell),
+cell(5, 1, emptyCell),
+cell(7, 1, emptyCell),
+cell(9, 1, emptyCell),
+cell(11, 1, emptyCell), 
+
+cell(0, 2, emptyCell),
+cell(2, 2, emptyCell),
+cell(4, 2, emptyCell),
+cell(6, 2, emptyCell),
+cell(8, 2, emptyCell),
+cell(10, 2, emptyCell), 
+cell(12, 2, emptyCell),
+
+cell(1, 3, emptyCell),
+cell(3, 3, emptyCell),
+cell(5, 3, emptyCell),
+cell(7, 3, emptyCell),
+cell(9, 3, emptyCell),
+cell(11, 3, emptyCell), 
+cell(13, 3, emptyCell),
+cell(15, 3, emptyCell),
+
+cell(0, 4, emptyCell),
+cell(2, 4, emptyCell),
+cell(4, 4, emptyCell),
+cell(6, 4, emptyCell),
+cell(8, 4, emptyCell),
+cell(10, 4, emptyCell), 
+cell(12, 4, emptyCell),
+cell(14, 4, emptyCell),
+cell(16, 4, emptyCell),
+
+cell(1, 5, emptyCell),
+cell(3, 5, emptyCell),
+cell(5, 5, emptyCell),
+cell(7, 5, emptyCell),
+cell(9, 5, emptyCell),
+cell(11, 5, emptyCell), 
+cell(13, 5, emptyCell),
+cell(15, 5, emptyCell),
+
+cell(0, 6, blackPiece),
+cell(2, 6, emptyCell),
+cell(4, 6, emptyCell),
+cell(6, 6, emptyCell),
+cell(8, 6, emptyCell),
+cell(10, 6, emptyCell), 
+cell(12, 6, emptyCell),
+
+cell(1, 7, emptyCell),
+cell(3, 7, emptyCell),
+cell(5, 7, emptyCell),
+cell(7, 7, emptyCell),
+cell(9, 7, emptyCell),
+cell(11, 7, emptyCell), 
+ 
+cell(0, 8, emptyCell),
+cell(2, 8, emptyCell),
+cell(4, 8, emptyCell),
+cell(6, 8, emptyCell),
+cell(8, 8, emptyCell)
+]).
+
+mini([
+  cell(0, 0, emptyCell),
+  cell(2, 0, blackPiece),
+  cell(4, 0, whitePiece),
+  cell(4, 1, whitePiece),
+  cell(4, 2, whitePiece)
+]).
+
+getPiece(Board, X, Y, Pout) :-
+  member(cell(X, Y, Pout), Board).
+
+setPiece(Board, X, Y, Pin, NewBoard) :-
+  select(cell(X, Y, _), Board, B1),
+  append([cell(X, Y, Pin)], B1, NewBoard).
+
+indices(List, E, Is) :-
+    findall(N, nth0(N, List, E), Is).
+
+getIndexs(_, [], _).
+getIndexs(Board, [H | T], List) :-
+  nth0(H, Board, Elem),
+  print(Elem), nl,
+  append(List, [Elem], L1),
+  getIndexs(Board, T, L1).
+
+getLine(Board, LineNumber, List) :-
+  indices(Board, cell(_, LineNumber, _), I),
+  getIndexs(Board, I, List).
+
+
+%  getLine([], _, _).
+%  getLine([H | T], LineNumber, List) :-
+%  H == cell(_, LineNumber,_) ->
+%  (write('eq'), nl,
+%  getLine(T, LineNumber, [List | H])) ;
+%  H \== cell(_, LineNumber,_) ->
+%  (write('neq'), nl,
+%  getLine(T, LineNumber, List)).
+
+print_line([]).
+print_line([L | T]) :-
+  print_cell(L),
+  write(' '),
+  print_line(T).
+  
+print_cell(cell(_, _, blackPiece)) :-
+    write('1').
+
+print_cell(cell(_, _, whitePiece)) :-
+    write('2').
+
+print_cell(cell(_, _, emptyCell)) :-
+    write('O').
+
