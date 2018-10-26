fedc1fc1492fed4dbe55bdcf110c3129190d2c51
Unknown <up201603634@fe.up.pt>
2018-10-26 16:49:43 +0100

    findCellNeighbors working
    more testing required
    is returning all the neighbors of a given colour, not only 5 or 6

--- Manalath/testSusy.pl
+++ Manalath/testSusy.pl
@@ -101,32 +101,54 @@ neighbor(Board, X, Y, _P, F, FX, FY) :-
   FY is Y + 1,
   getPiece(Board, FX, FY, F).
 
-findFirstNeighbors(Board,X,Y,_P,Color,OldRes, Res) :-
+%guarda no ToProcess os vizinhos da cell(X,Y,_P)
+findFirstNeighbors(Board,X,Y,_P,Color,Processed,ToProcess) :-
  findall((FX,FY),
   (
+  !,
   neighbor(Board,X,Y,_P,F1,FX,FY),
   F1 == Color,
-  \+ member((FX,FY),OldRes)
+  \+ member((FX,FY), Processed)
   ),
- Res).
+  ToProcess).
 
-findAllNeighbors(Board,T,Color,Res) :-
-  length(T,N),
-  N >= 6,!,
-  Res = T.
+%quando ja nao ha mais celulas para processar termina
+findAllNeighbors(Board,Color,ToProcess,ProcessedCells,Res) :-
+  length(ToProcess,N),
+  N == 0, !,
+  Res = ProcessedCells.
 
-findAllNeighbors(Board,[(SX,SY)|T],Color,Res) :-
-  %member((SX,SY), FirstNeighbors),
-  findFirstNeighbors(Board,SX,SY,_S,Color,[(SX,SY)|T],Tmp),
-  append(T,Tmp,R),
-  findAllNeighbors(Board,R,Color,Res).
+%processa o primeiro elemento da lista ToProcess ((SX,SY))
+%guarda (SX,SY) nos Processed
+%guarda o resto no ToProcess
+%Funciona? nao....
+findAllNeighbors(Board,Color,[(SX,SY)|T],ProcessedCells,Res) :-
+  findFirstNeighbors(Board,SX,SY,_S,Color,ProcessedCells,Tmp),
+  append(T,Tmp,ToProcess),
+  sort(ToProcess,TP),
+  append(ProcessedCells,[(SX,SY)], PC),
+  findAllNeighbors(Board,Color,TP, PC,Res).
 
+findCellNeighbors(Board,X,Y,Color) :-
+  findAllNeighbors(Board,Color,[(X,Y)],[],Res),
+  printLongList(Res).
+
+printLongList(List) :-
+  length(List,N),
+  N == 0.
+printLongList([L|T]) :-
+  write('('),
+  write(L),
+  write(')'),
+  write(' '),
+  printLongList(T).
+
+%guardar os ja processados noutra lista e comparar
+%no findFirstNeighbors
 
 findNeighbors(Board,X,Y,_P,Color,OldRes,FirstN,Res) :-
   findFirstNeighbors(Board,X,Y,_P,Color,OldRes, FirstN),
   append(OldRes,FirstN,Tmp),
-  %length(Res,N),
-  %N < 4.9,
   member((SX,SY), FirstN),
   findFirstNeighbors(Board,SX,SY,_S,Color,Tmp,T1),
   append(T1,Tmp,Res).
