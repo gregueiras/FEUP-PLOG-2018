835d792e39467a8ba7a8592af203d825f176bc90
Unknown <up201603634@fe.up.pt>
2018-10-27 17:00:06 +0100

    some work on playing and validating plays (WIP)

--- Manalath/main.pl
+++ Manalath/main.pl
@@ -6,12 +6,33 @@ display_game(Board, Player) :-
 tp :-
   initial_board(_B), print_board(_B).
 
+
+
 %mudar de lugar, para ja meti aqui por causa do includes (nao me odeies dps tiro)
-%o setPiece nao funciona acho, apaga a celula
+
+checkPlay(Board,Player,Color,Count, ValidPlay) :-
+  Count >= 6,
+  ValidPlay = -1. %jogada invalida, arranjar uma cena mais bonitinha maybe
+
+checkPlay(Board,Player,Color,Count, ValidPlay) :-
+  checkColorPlayer(Player,Color),
+  Count == 5,
+  ValidPlay = 0. %ganhou, arranjar uma cena mais bonitinha maybe
+
+checkPlay(Board,Player,Color,Count, ValidPlay) :-
+  checkColorPlayer(Player,Color),
+  Count == 4,
+  ValidPlay = 1. %perdeu, arranjar uma cena mais bonitinha maybe
+
+checkPlay(Board,Player,Color,Count, ValidPlay) :-
+  ValidPlay = 2. %jogada valida, arranjar uma cena mais bonitinha maybe
+
+%o setPiece nao funciona acho, apaga a celula...
 %nao valida a jogada ainda
-%deve precisar de um cut algures
 play(Board, Player, NewBoard, NewPlayer, X,Y,Color) :-
   setPiece(Board, X, Y, Color, NewBoard), %falta validar se a celula esta vazia
-  countCellNeighbors(NewBoard,X,Y,Color,Count), %falta validar se ganha, perde ou se e invalida
-  switchCurrentPlayer(Player,NewPlayer),%troca o player atual
-  display_game(NewBoard,NewPlayer). %imprime o novo tabuleiro
+  countCellNeighbors(NewBoard,X,Y,Color,NrNeighbors), %falta validar se ganha, perde ou se e invalida
+  checkPlay(NewBoard,Player,Color,NrNeighbors,ValidPlay), %usar o validPlay para preparar a proxima jogada ou ver o fim de jogo
+  switchCurrentPlayer(Player,NewPlayer),%troca o player atual se a jogada for valida
+  display_game(NewBoard,NewPlayer), %imprime o novo tabuleiro
+  !.
