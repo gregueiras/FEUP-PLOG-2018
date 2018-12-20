451937ecae9cc515b28bccd15be29c4a2bd089db
Unknown <up201603634@fe.up.pt>
2018-12-20 09:03:13 +0000

    auxiliar and menus documented

--- Doors/auxiliar.pl
+++ Doors/auxiliar.pl
@@ -7,11 +7,15 @@
 %%%                    Auxiliary functions                        %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
+% getPiece(+Board, +X, +Y)
+% True if there is a cell in Board with X and Y coordinates
 getPiece(Board, X, Y) :-
   member(cell(X, Y), Board).
 
-
+% getFrontier(+X1, +Y1, +X2, +Y2, +Frontiers, -Value)
+% gets the value from the frontier with coordinates X1,Y1,X2,Y2 (and vice versa)
+% Frontiers is a list in the format  [FrontCoords, FrontValues], being FrontCoords the list of 
+% coordinates and FrontValues the list of values.
 getFrontier(X1, Y1, X2, Y2, [FrontCoords, FrontValues], Value) :-
   nth1(Index, FrontCoords, frontier(X1,Y1,X2,Y2)),
   element(Index, FrontValues, Value),!.
@@ -21,38 +25,61 @@ getFrontier(X1, Y1, X2, Y2, [FrontCoords, FrontValues], Value) :-
   element(Index, FrontValues, Value), !.
 
 
+% getFrontierRightDown(+X1, +Y1, +X2, +Y2, +Frontiers, Value) 
+% gets the value from the frontier with coordinates X1,Y1,X2,Y2, corresponding to a down or right frontier
+% Frontiers is a list in the format  [FrontCoords, FrontValues], being FrontCoords the list of 
+% coordinates and FrontValues the list of values.
 getFrontierRightDown(X1, Y1, X2, Y2, [FrontCoords, FrontValues], Value) :-
   nth1(Index, FrontCoords, frontier(X1,Y1,X2,Y2)),
   element(Index, FrontValues, Value).
 
+% getFrontierLeftUp(+X1, +Y1, +X2, +Y2, +Frontiers, Value) 
+% gets the value from the frontier with coordinates X2,Y2,X1,Y1, corresponding to an up or left frontier
+% Frontiers is a list in the format  [FrontCoords, FrontValues], being FrontCoords the list of 
+% coordinates and FrontValues the list of values.
 getFrontierLeftUp(X1, Y1, X2, Y2, [FrontCoords, FrontValues], Value) :-
   nth1(Index, FrontCoords, frontier(X2,Y2,X1,Y1)),
   element(Index, FrontValues, Value).
 
+
+% neighborDown(+Board, +X, +Y,-RX,-RY)
+% Gets the down neighbor from a cell with X and Y coordinates in Board
+% Neighbor has coordinates FX and FY 
 neighborDown(Board,X,Y,RX,RY) :-
   getPiece(Board,X,Y),
   RX is X+1,
   RY is Y,
   getPiece(Board,RX,RY).
 
+% neighborDown(+Board, +X, +Y,-RX,-RY)
+% Gets the above neighbor from a cell with X and Y coordinates in Board
+% Neighbor has coordinates FX and FY 
 neighborUp(Board,X,Y,RX,RY) :-
   getPiece(Board,X,Y),
   RX is X-1,
   RY is Y,
   getPiece(Board,RX,RY).
 
+% neighborDown(+Board, +X, +Y,-RX,-RY)
+% Gets the right neighbor from a cell with X and Y coordinates in Board
+% Neighbor has coordinates FX and FY 
 neighborRight(Board,X,Y,RX,RY) :-
   getPiece(Board,X,Y),
   RX is X,
   RY is Y+1,
   getPiece(Board,RX,RY).
 
+% neighborDown(+Board, +X, +Y,-RX,-RY)
+% Gets the left neighbor from a cell with X and Y coordinates in Board
+% Neighbor has coordinates FX and FY 
 neighborLeft(Board,X,Y,RX,RY) :-
   getPiece(Board,X,Y),
   RX is X,
   RY is Y-1,
   getPiece(Board,RX,RY).
 
+% getDownFrontiers(+Board,+X,+Y,+Frontiers,+Values, +AccDownFrontiers,-DownFrontiers)
+% gets all the down frontiers values visible from the X,Y cell of the Board
 getDownFrontiers(Board,X,Y,Frontiers,Values, AccDownFrontiers,DownFrontiers) :-
   neighborDown(Board,X,Y,FX,FY),
   getFrontierRightDown(X,Y,FX,FY,[Frontiers,Values], Frontier),
@@ -60,6 +87,8 @@ getDownFrontiers(Board,X,Y,Frontiers,Values, AccDownFrontiers,DownFrontiers) :-
   getDownFrontiers(Board,FX,FY,Frontiers,Values,NewAccDF,DownFrontiers),!.
 getDownFrontiers(_,_,_,_,_, AccDownFrontiers,AccDownFrontiers) :- !.
 
+% getUpFrontiers(+Board,+X,+Y,+Frontiers,+Values, +AccDownFrontiers,-DownFrontiers)
+% gets all the up frontiers values visible from the X,Y cell of the Board
 getUpFrontiers(Board,X,Y,Frontiers,Values, AccUpFrontiers,UpFrontiers) :-
   neighborUp(Board,X,Y,FX,FY),
   getFrontierLeftUp(X,Y,FX,FY,[Frontiers,Values], Frontier),
@@ -67,6 +96,8 @@ getUpFrontiers(Board,X,Y,Frontiers,Values, AccUpFrontiers,UpFrontiers) :-
   getUpFrontiers(Board,FX,FY,Frontiers,Values,NewAccUF,UpFrontiers).
 getUpFrontiers(_,_,_,_,_, AccUpFrontiers,AccUpFrontiers) .
 
+% getLeftFrontiers(+Board,+X,+Y,+Frontiers,+Values, +AccDownFrontiers,-DownFrontiers)
+% gets all the left frontiers values visible from the X,Y cell of the Board
 getLeftFrontiers(Board,X,Y,Frontiers,Values, AccLeftFrontiers,LeftFrontiers) :-
   neighborLeft(Board,X,Y,FX,FY),
   getFrontierLeftUp(X,Y,FX,FY,[Frontiers,Values], Frontier),
@@ -74,6 +105,8 @@ getLeftFrontiers(Board,X,Y,Frontiers,Values, AccLeftFrontiers,LeftFrontiers) :-
   getLeftFrontiers(Board,FX,FY,Frontiers,Values,NewAccLF,LeftFrontiers),!.
 getLeftFrontiers(_,_,_,_,_, AccLeftFrontiers,AccLeftFrontiers) :- !.
 
+% getRightFrontiers(+Board,+X,+Y,+Frontiers,+Values, +AccDownFrontiers,-DownFrontiers)
+% gets all the right frontiers values visible from the X,Y cell of the Board
 getRightFrontiers(Board,X,Y,Frontiers,Values, AccRightFrontiers,RightFrontiers) :-
   neighborRight(Board,X,Y,FX,FY),
   getFrontierRightDown(X,Y,FX,FY,[Frontiers,Values], Frontier),
@@ -81,11 +114,14 @@ getRightFrontiers(Board,X,Y,Frontiers,Values, AccRightFrontiers,RightFrontiers) 
   getRightFrontiers(Board,FX,FY,Frontiers,Values,NewAccRF,RightFrontiers),!.
 getRightFrontiers(_,_,_,_,_, AccRightFrontiers,AccRightFrontiers) :- !.
 
-
+% getCellValue(+Board,+X,+Y,+CellValues,-Value)
+% gets the corresponding value of the cell X,Y in the Board in CellValues list
 getCellValue(Board,X,Y,CellValues,Value) :-
   nth1(Index, Board, cell(X,Y)),
   element(Index, CellValues, Value).
 
+% print_long_list(+List)
+% prints a long list
 print_long_list([]) .  
 print_long_list([H|T]) :-
     write(H), write('  '),
@@ -112,6 +148,8 @@ processChar(Char, Acc, Option) :-
   atom_concat(Acc, Char, Res),
   new_read(Res, Option).
 
+% getInteger(+List, +Acc, -Integer) 
+% gets and Integer from a List of codes (format: ['1','2','3'])
 getInteger([], Acc, Acc).
 getInteger([H|T], Acc, Integer) :-
   length([H|T], L),
@@ -123,6 +161,8 @@ getInteger([H|T], Acc, Integer) :-
   Acc1 is Acc + CC1*Factor,
   getInteger(T,Acc1,Integer).
 
+% validate_code(+Code)
+% Validates if a Code is a digit between 0 and 9
 validate_code(Code) :-
   Code = 0.
 validate_code(Code) :-
@@ -144,12 +184,14 @@ validate_code(Code) :-
 validate_code(Code) :-
   Code = 9.
 
-  
+% power(+B,+P,-R)
+% calculates the B power to P
 power(_,0,1) :-!.
 power(B,1,B) :-!.
 power(B,P,R) :-
   power(B,B,P,1,R), !.
-
+% power(B,Bc,P,Acc,R)
+% auxiliary function of power(B,P,R), calculates the B power to P
 power(_,Bc,P,P,Bc).
 power(B,Bc,P,Acc,R) :-
     B1 is Bc*B,
@@ -165,7 +207,8 @@ print_InvalidOption :-
 %%%                         draw board                            %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
+% getLinesN(+List,+N, +Count, +TmpList, +FinalList, -Res)
+% divides a List in sublist of N lenght
 getLinesN([],_, _, _, FinalList, FinalList) :- !.
 
 getLinesN([H|T],N, Count, TmpList, FinalList, Res) :-
@@ -180,13 +223,19 @@ getLinesN([H|T],N, Count, TmpList, FinalList, Res) :-
   NewCount is Count+1,
   getLinesN(T,N,NewCount,NewTmpList,FinalList,Res),!.
 
-
+% draw_board(+CellCoordinates,+CellValues,+Frontiers,+N)
+% Prints the puzzle Board
+% CellCoordinates is the list of cells in the format cell(X,Y)
+% CellValues is the list of corresponding values for the cells in the previous list
+% Frontiers is a list in the format  [FrontCoords, FrontValues], being FrontCoords the list of 
+% coordinates and FrontValues the list of values.
 draw_board([H|T],CellValues,Frontiers,N) :-
     nl,
     getLinesN([H|T],N,0,[],[],Lines),
     drawLines([H|T],Lines, CellValues, Frontiers), !.
  
-
+% drawLines(+Board,+Lines, +CellValues,+Frontiers)
+% draws a list of Lines from the board Board
 drawLines(_,[],_, _).
 
 drawLines(Board,[H|T], CellValues,Frontiers) :-
@@ -194,6 +243,8 @@ drawLines(Board,[H|T], CellValues,Frontiers) :-
   draw_horizontal_frontiers(H,Frontiers),
   drawLines(Board,T,CellValues,Frontiers).
 
+% draw_line(+Board,+Line,+CellValues,+Frontiers) 
+% draws all the cells and vertical frontiers from a Line of the board Board
 draw_line(_,[],_,_) :-
       nl.
 
@@ -204,24 +255,23 @@ draw_line(Board,[cell(X1,Y1)|T],CellValues,Frontiers) :-
      draw_vertical_frontier(Frontiers,X1,Y1,X1,Y2),
      draw_line(Board,T,CellValues,Frontiers).
 
+% draw_cell(+Value)
+% draws a cell of value Value
 draw_cell(Value) :-
   Value < 10,
   write(' ') ,write(Value), write(' ').
-
-
 draw_cell(Value) :-
   write(' ') ,write(Value), write('').
 
-
+% draw_horizontal_frontiers(+Line,+Frontiers)
+% draws all the horizontal frontiers from a Line
 draw_horizontal_frontiers([],_) :-
        nl.
-
 
 draw_horizontal_frontiers([cell(X1,Y1)|T],Frontiers) :-
      X2 is X1 +1 ,
     draw_horizontal_frontier(Frontiers,X1,Y1,X2,Y1),
     draw_horizontal_frontiers(T,Frontiers).
-
 
 draw_horizontal_frontier(Frontiers,X1,Y1,X2,Y2) :-
     getFrontier(X1,Y1,X2,Y2,Frontiers, 1),
@@ -230,6 +280,8 @@ draw_horizontal_frontier(Frontiers,X1,Y1,X2,Y2) :-
 draw_horizontal_frontier(_,_,_,_,_) :-
     write('     ').
 
+% draw_vertical_frontier(+Frontiers, +X1, +Y1, +X2,+Y2)
+% draws the horizontal frontiers between the cell(X1,Y1) and the cell(X2,Y2)
 draw_vertical_frontier(Frontiers,X1,Y1,X2,Y2) :-
     getFrontier(X1,Y1,X2,Y2,Frontiers, 1),
     write(' |').
--- Doors/menus.pl
+++ Doors/menus.pl
@@ -5,7 +5,7 @@
 %%%                         Start Function                        %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
+%start function
 start :-
   firstMenu.
 
@@ -14,75 +14,77 @@ start :-
 %%%                             Menus                             %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-  
-  firstMenu :-
-    print_FirstMenu,
-    new_read(Option),
-    firstMenu(Option).
-  
-  
-  print_FirstMenu :-
-    nl,
-    write('******************************'),nl,
-    write('**          DOORS           **'),nl,
-    write('******************************'),nl,
-    write('**                          **'),nl,
-    write('** Board Size:              **'),nl,
-    write('** 1- 8x8                   **'),nl,
-    write('** 2- 16x16                 **'),nl,
-    write('** 3- 24x24                 **'),nl,
-    write('** 4- Make your own!!!      **'),nl,
-    write('** 5- quit                  **'),nl,
-    write('**                          **'),nl,
-    write('******************************'),nl,
-    write('Choose : '), nl.
-  
-  
-  firstMenu('1') :-
-    nl,
-    generatorAndSolver(8),
-    firstMenu.
-  
-  firstMenu('2') :-
-    nl,
-    generatorAndSolver(16),
-    firstMenu.
-  
-  firstMenu('3') :-
-    nl,
-    generatorAndSolver(24),
-    firstMenu.
+% firstMenu
+% Prints the initial menu and reads the user input
+firstMenu :-
+  print_FirstMenu,
+  new_read(Option),
+  firstMenu(Option).
 
-  firstMenu('4') :-
-    nl, 
-    cutomizeSizeMenu.
+firstMenu('1') :-
+  nl,
+  generatorAndSolver(8),
+  firstMenu.
+
+firstMenu('2') :-
+  nl,
+  generatorAndSolver(16),
+  firstMenu.
+
+firstMenu('3') :-
+  nl,
+  generatorAndSolver(24),
+  firstMenu.
+
+firstMenu('4') :-
+  nl, 
+  cutomizeSizeMenu.
+
+firstMenu('5').
+
+firstMenu(_) :-
+  print_InvalidOption,
+  firstMenu.
+
+% print_FirstMenu
+print_FirstMenu :-
+  nl,
+  write('******************************'),nl,
+  write('**          DOORS           **'),nl,
+  write('******************************'),nl,
+  write('**                          **'),nl,
+  write('** Board Size:              **'),nl,
+  write('** 1- 8x8                   **'),nl,
+  write('** 2- 16x16                 **'),nl,
+  write('** 3- 24x24                 **'),nl,
+  write('** 4- Make your own!!!      **'),nl,
+  write('** 5- quit                  **'),nl,
+  write('**                          **'),nl,
+  write('******************************'),nl,
+  write('Choose : '), nl.
 
 
-  firstMenu('5').
+% cutomizeSizeMenu
+% Prints the customize board size menu and reads the user input
+cutomizeSizeMenu :-
+  print_CutomizeSizeMenu,
+  new_read(Option),
+  atom_chars(Option,Chars),
+  getInteger(Chars,0,Size),
+  generatorAndSolver(Size),
+  firstMenu.
 
-  firstMenu(_) :-
-    print_InvalidOption,
-    firstMenu.
+cutomizeSizeMenu :-
+  print_InvalidOption,
+  firstMenu.
 
-
-  cutomizeSizeMenu :-
-    print_CutomizeSizeMenu,
-    new_read(Option),
-    atom_chars(Option,Chars),
-    getInteger(Chars,0,Size),
-    generatorAndSolver(Size),
-    firstMenu.
-
-  cutomizeSizeMenu :-
-    print_InvalidOption,
-    firstMenu.
-
-  print_CutomizeSizeMenu :-
-    nl,
-    write('******************************'),nl,
-    write('**   CUSTOMIZE BOARD SIZE   **'),nl,
-    write('******************************'),nl, nl,
-    write('Board Size: '), nl.
+% print_CutomizeSizeMenu
+print_CutomizeSizeMenu :-
+  nl,
+  write('******************************'),nl,
+  write('**   CUSTOMIZE BOARD SIZE   **'),nl,
+  write('******************************'),nl, nl,
+  write('Board Size: '), nl.
 
 
 
